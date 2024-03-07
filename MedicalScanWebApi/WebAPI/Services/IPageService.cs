89c2532e01ff3cb70af07375b25eea3da74bd806
Bence Nagy <nagybence0601@gmail.com>
2024-03-07 14:18:57 +0100

    Add : ProductController, ProductModel, and an Interface for declaring CRUD operations

---
+++ MedicalScanWebApi/WebAPI/Services/IPageService.cs
@@ -0,0 +1,14 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public interface IPageService
+    {
+        public Task<List<ProductModel>> GetAllProducts();
+        public Task<ProductModel> GetProductById(int  id);
+        public Task<ProductModel> UpdateProduct(int id, ProductModel model);
+        public Task<List<ProductModel>> CreateNewProduct(ProductModel model);
+        public Task<List<ProductModel>> DeleteProduct(int id);
+
+    }
+}
