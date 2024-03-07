67dec607ff3cec6b14be20b77695278defb93127
Bence Nagy <nagybence0601@gmail.com>
2024-03-07 17:28:14 +0100

    Add : Error and exception handling in PageController class

--- MedicalScanWebApi/WebAPI/Controllers/PageController.cs
+++ MedicalScanWebApi/WebAPI/Controllers/PageController.cs
@@ -1,30 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using WebAPI.Models;
 using WebAPI.Services;
 
 namespace WebAPI.Controllers
 {
-    
-        [ApiController]
-        [Route("[controller]")]
-        public class PageController : ControllerBase
+    [ApiController]
+    [Route("[controller]")]
+    public class PageController : ControllerBase
+    {
+        private readonly IPageService _pageService;
+
+        public PageController(IPageService pageService)
         {
-            private readonly IPageService _pageService;
+            _pageService = pageService;
+        }
 
-            public PageController(IPageService pageService)
-            {
-                _pageService = pageService;
-            }
-
-            [HttpGet("Products")]
-            public async Task<ActionResult<List<ProductModel>>> GetAllProducts()
+        [HttpGet("Products")]
+        public async Task<ActionResult<List<ProductModel>>> GetAllProducts()
+        {
+            try
             {
                 var products = await _pageService.GetAllProducts();
                 return Ok(products);
             }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving products.");
+            }
+        }
 
-            [HttpGet("Product/{id}")]
-            public async Task<ActionResult<ProductModel>> GetProductById(int id)
+        [HttpGet("Product/{id}")]
+        public async Task<ActionResult<ProductModel>> GetProductById(int id)
+        {
+            try
             {
                 var product = await _pageService.GetProductById(id);
                 if (product == null)
@@ -33,16 +46,32 @@ namespace WebAPI.Controllers
                 }
                 return Ok(product);
             }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving the product.");
+            }
+        }
 
-            [HttpPost("CreateProduct")]
-            public async Task<ActionResult<List<ProductModel>>> CreateProduct(ProductModel model)
+        [HttpPost("CreateProduct")]
+        public async Task<ActionResult<List<ProductModel>>> CreateProduct(ProductModel model)
+        {
+            try
             {
                 var products = await _pageService.CreateNewProduct(model);
                 return Ok(products);
             }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the product.");
+            }
+        }
 
-            [HttpPut("UpdateProduct/{id}")]
-            public async Task<ActionResult<ProductModel>> UpdateProduct(int id, ProductModel model)
+        [HttpPut("UpdateProduct/{id}")]
+        public async Task<ActionResult<ProductModel>> UpdateProduct(int id, ProductModel model)
+        {
+            try
             {
                 var updatedProduct = await _pageService.UpdateProduct(id, model);
                 if (updatedProduct == null)
@@ -51,13 +80,26 @@ namespace WebAPI.Controllers
                 }
                 return Ok(updatedProduct);
             }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while updating the product.");
+            }
+        }
 
-            [HttpDelete("DeleteProduct/{id}")]
-            public async Task<ActionResult<List<ProductModel>>> DeleteProduct(int id)
+        [HttpDelete("DeleteProduct/{id}")]
+        public async Task<ActionResult<List<ProductModel>>> DeleteProduct(int id)
+        {
+            try
             {
                 var products = await _pageService.DeleteProduct(id);
                 return Ok(products);
             }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while deleting the product.");
+            }
         }
-    
+    }
 }
