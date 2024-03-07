61bdd4096b0b9563dd1c338aff5ea0803391d4fd
Bence Nagy <nagybence0601@gmail.com>
2024-03-07 14:22:41 +0100

    Add : Implemented missing interface members

--- MedicalScanWebApi/WebAPI/Services/PageService.cs
+++ MedicalScanWebApi/WebAPI/Services/PageService.cs
@@ -1,6 +1,32 @@
-﻿namespace WebAPI.Services
+﻿using WebAPI.Models;
+
+namespace WebAPI.Services
 {
-    public class PageService 
-    { 
+    public class PageService : IPageService
+    {
+        public Task<List<ProductModel>> CreateNewProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductModel>> DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductModel>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductModel> GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductModel> UpdateProduct(int id, ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
